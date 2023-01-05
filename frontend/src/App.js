7ed8a018977912a2eb209e4768b81fa151dab447
vhivestate <garcia.valeria001@gmail.com>
2023-01-04 20:16:16 -0700

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-    <h1>Welcome To Plant Shop</h1>
+      <Header/>
+      <main>
+        <Container>
+          <h1>Welcome To Plant Shop</h1>
+        </Container>
+      </main>  
+      <Footer/>
     </>
   );
 }
