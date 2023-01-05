b6aa43b0642ec7a85db7b37de6cf0ede5fca90da
vhivestate <garcia.valeria001@gmail.com>
2023-01-04 21:50:15 -0700

    product component

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import products from "../products"
 import { Row, Col } from 'react-bootstrap'
+import products from "../products"
+import Product from '../components/Product'
 
 const HomeScreen = () => {
   return (
@@ -9,7 +10,8 @@ const HomeScreen = () => {
         <Row>
           {products.map(product => (
             <Col sm={12} md={6} lg={4} xl={3}>
-                <h3>{product.name}</h3>
+            {/* render components */}
+                <Product product={product}/>
             </Col>
           ))}
         </Row>
