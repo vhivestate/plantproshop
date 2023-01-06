1fafa153e5e8f708d34e3a91290c4a253daf77ff
vhivestate <garcia.valeria001@gmail.com>
2023-01-05 20:16:28 -0700

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,19 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './pages/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header/>
       <main className='py-3'>
         <Container>
-          <HomeScreen/>          
+          <Route path='/' component={HomeScreen} exact />          
         </Container>
       </main>  
       <Footer/>
-    </>
+    </Router>
   );
 }
 
