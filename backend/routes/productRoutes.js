aedf2fc1512cd38a90e9bf4f0019d44dbfa4c01c
vhivestate <garcia.valeria001@gmail.com>
2023-01-11 14:50:30 -0700

    custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,14 +15,18 @@ router.get('/', asyncHandler(async (req, res) => {
 // @desc fetch single product
 // @route GET /api/products/:id
 // @access Public
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get(
+    '/:id', 
+    asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
         if (product) {
             res.json(product)
         } else {
-            res.status(404).json({ message: 'Product not found'})
+            res.status(404)
+            throw new Error('Product not found')
         }
-}))
+    })
+)
 
 export default router
