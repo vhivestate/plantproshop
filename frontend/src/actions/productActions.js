5d5efd473f8c30fb55e7e2d50fb5ca972a1f5adb
vhivestate <garcia.valeria001@gmail.com>
2023-01-14 23:44:47 -0700

    bringing redux state into homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import { 
     PRODUCT_LIST_SUCCESS, 
     PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
 } from "../constants/productConstant"
 
 export const listProducts = () => async (dispatch) => {
     try {
-        dispatch({  type: PRODUCT_LIST_REQUEST })
+        dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await
+        const { data } = await axios.get('/api/products')
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
