364c6028d3002d85f1d687a7f74cadb27850da2c
vhivestate <garcia.valeria001@gmail.com>
2023-01-14 21:18:02 -0700

    product list reducer/actions

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,3 +1,9 @@
+import { 
+    PRODUCT_LIST_SUCCESS, 
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_FAIL
+} from "../constants/productConstant"
+
 export const productListReducer = ( state = { products: [] }, action) => {
     switch (action.type) {
         case 'PRODUCT_LIST_REQUEST':
